acf15328318b8c608b9220a18dc1cd2a19b169ea
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-03-09 12:43:29 -0500

    Basic setup of Email Class

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,4 +1,40 @@
 package emailapp;
 
 public class Email {
+
+	private String firstName;
+	private String lastName;
+	private String department;
+	private String email;
+	private String password;
+
+	public Email(String firstName, String lastName, String department) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.department = department;
+		this.email = firstName + "." + lastName + "@" + department + ".company.com";
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+		this.email = firstName + "." + lastName + "@" + department + ".company.com";
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+
 }
